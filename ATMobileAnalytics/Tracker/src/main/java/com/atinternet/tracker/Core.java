536816e77ff87048cfcc427df0ff2521db4c24e3
Nicolas Sagnette <nicolas.sagnette@atinternet.com>
2019-10-02 12:03:13 +0200

    Bump patch version

--- ATMobileAnalytics/Tracker/src/main/java/com/atinternet/tracker/Core.java
+++ ATMobileAnalytics/Tracker/src/main/java/com/atinternet/tracker/Core.java
@@ -1122,7 +1122,7 @@ class TechnicalContext {
     static final Closure VTAG = new Closure() {
         @Override
         public String execute() {
-            return "2.13.2";
+            return "2.13.3";
         }
     };
 
